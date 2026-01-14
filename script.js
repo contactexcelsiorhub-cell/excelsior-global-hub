7e6b98e1ae4ccea5a0e46b6bb77671d1a0c1648f
contactexcelsiorhub-cell <contact.excelsiorhub@gmail.com>
2026-01-15 03:36:06 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<script src="script.js"></script>
+// Simple interactivity: highlight venture on click
+document.addEventListener("DOMContentLoaded", () => {
+  const ventures = document.querySelectorAll("ul li");
+  ventures.forEach(item => {
+    item.addEventListener("click", () => {
+      alert(`You selected: ${item.textContent}`);
+    });
+  });
+});
+
